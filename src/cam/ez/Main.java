67457512194a04b1575dbd0e3202a3ee65a4eae0
69hr <57600549+69hr@users.noreply.github.com>
2020-11-25 02:44:33 -0500

    huge code, wacth out! :^)

---
+++ src/cam/ez/Main.java
@@ -0,0 +1,103 @@
+package cam.ez;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+
+    public static void main(String[] args)
+
+    {
+        mhm("EZZnnDOWN");
+
+        mhm("noobisez");
+
+        System.out.println("yoink teach camer code ez");
+
+        int cam = 3;
+
+        String camer = "hi";
+
+        System.out.println(camer);
+
+        learn();
+
+        arry();
+
+        listez();
+
+        nn();
+
+        //ezz();
+    }
+
+    public static void learn()
+    {
+
+        System.out.println("yoink is yes");
+
+
+    }
+
+    public static void arry()
+    {
+
+        int[] yes = {1, 2, 3, 4};
+
+        System.out.println(yes[0]);
+
+        String[] ez = {"yes", "yoink", "yoinkisyes" };
+
+        for (String pvp : ez )
+        {
+            System.out.println(pvp);
+        }
+
+    }
+
+    public static void listez ()
+    {
+
+        List<String> hacks = new ArrayList<>();
+
+        hacks.add("I AM PVP GOD");
+
+        hacks.add("I WILL EZZ ALL NOOBS");
+
+        hacks.add("xanax on top!");
+
+        for (String cpvp : hacks)
+
+        {
+            System.out.println(cpvp);
+        }
+
+    }
+
+    public static void nn ()
+    {
+
+
+    }
+
+        public static void yoinks(String[] args) {
+
+            mhm("EZZnnDOWN");
+
+            mhm("noobisez");
+
+        }
+    public static void mhm(String SixNineSays)
+    {
+        System.out.println(SixNineSays);
+    }
+
+
+
+}
+
+
+
+
+
